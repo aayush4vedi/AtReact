870438472fdb2836792e8ff184df9a29231a2910
@4vedi <aayush.chaturvedi@razorpay.com>
2021-05-24 15:23:28 +0530

    [2]. Firestore, ChakraUI, absolute import (#4)
    * modified readme
    
    * updated header
    
    * Added firbase auth for github login
    
    * #1: Minor Refactoring
    
    * Update auth.js
    
    * Update auth.js
    
    * Update auth.js
    
    * #2: firestore+ chakraUI import

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,32 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { auth } from 'firebase';
-import { useAuth } from '../lib/auth';
+import { Button, Text, Heading, Code } from '@chakra-ui/react';
+
+import { useAuth } from '@/lib/auth';
 
 const Home = () => {
   //use auth hook created
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>At React</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <button onClick={(e) => auth.signinWithGithub()}>Sing In</button>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          At<a href="https://nextjs.org">React!</a>
-        </h1>
+      <main>
+        <Heading fontWeight="medium">AtReact!</Heading> 
 
-        <p className={styles.description}>
-          A New Way to Send & Receive Feedback
-        </p>
         {/* //this '?.' is kind of ternary opertor provided by NextJS */}
-        <div>Current user: {auth.user ? auth.user.email : 'None'}</div>
+        <Text>
+          Current user: <Code> {auth.user ? auth.user.email : 'None'} </Code>
+        </Text>
 
         {auth.user ? (
-          <button onClick={(e) => auth.signout()}>Sign Out</button>
+          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
         ) : (
-          <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
+          <Button onClick={(e) => auth.signinWithGitHub()}>Sign In</Button>
         )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 };
