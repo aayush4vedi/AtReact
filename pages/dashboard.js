357ca57dd2668db132e4b931032ecaca1bb18cbb
aayush4vedi <4vedi.aayush@gmail.com>
2021-05-26 04:28:15 +0530

    Added firebase-admin server side

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,13 +2,23 @@ import Head from 'next/head';
 
 import { useAuth } from '@/lib/auth';
 import EmptyState from '@/components/EmptyState';
+import SiteTableSkeleton from '../components/SiteTableSkeleton';
+import DashboardShell from '../components/DashboardShell';
 
 const Dashboard = () => {
   const auth = useAuth();
 
-//   if (!auth.user) {
-//     return 'Loading...';
-//   }
+  //   if (!auth.user) {
+  //     return 'Loading...';
+  //   }
+
+  if (!auth.user) {
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton />
+      </DashboardShell>
+    );
+  }
 
   return <EmptyState />;
 };
