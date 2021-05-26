490e3b8c30d338acd088ca569a585a1af04ca2ef
aayush4vedi <4vedi.aayush@gmail.com>
2021-05-27 02:10:30 +0530

    #5. Creating Feedback Pages

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,12 +1,12 @@
 import db from '@/lib/firebase-admin';
+import { getAllSites } from '@/lib/db-admin';
 
-export default async (_, res) => {    // `_` in place of `req` because we're not using 'req' values
-  const snapshot = await db.collection('sites').get();
-  const sites = [];
+export default async (_, res) => {
+  const { sites, error } = await getAllSites();
 
-  snapshot.forEach((doc) => {
-    sites.push({ id: doc.id, ...doc.data() });
-  });
+  if (error) {
+    res.status(500).json({ error });
+  }
 
   res.status(200).json({ sites });
 };
