2ee37fe3a77aef47eb49f5f65b9331ea75be2aad
@4vedi <aayush.chaturvedi@razorpay.com>
2021-05-28 00:22:41 +0530

    #7.  Add feedback page and sign in with Google (#9)

---
+++ components/SiteTableHeader.js
@@ -0,0 +1,25 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Heading,
+  Flex
+} from '@chakra-ui/react';
+
+import AddSiteModal from './AddSiteModal';
+
+const SiteTableHeader = () => (
+  <>
+    <Breadcrumb>
+      <BreadcrumbItem>
+        <BreadcrumbLink>Sites</BreadcrumbLink>
+      </BreadcrumbItem>
+    </Breadcrumb>
+    <Flex justifyContent="space-between">
+      <Heading mb={8}>My Sites</Heading>
+      <AddSiteModal>+ Add Site</AddSiteModal>
+    </Flex>
+  </>
+);
+
+export default SiteTableHeader;
